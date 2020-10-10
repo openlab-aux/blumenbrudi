8a5856a2acf89165ce68b6cb33e6957f5a4a518c
HASS User <hass@openlab-augsburg.de>
2020-10-11 01:32:42 +0200

    enable prometheus endpoint on plug5

--- esphome/chirp.h
+++ esphome/chirp.h
@@ -1,25 +1,25 @@
 #include "esphome.h"
 
-class ChirpComponent : public PollingComponent, public Sensor {
+class ChirpComponent : public PollingComponent, public Sensor, public I2CDevice {
 	public:
 		ChirpComponent() : PollingComponent(5000) {}
 
 		void setup() override {
-			Wire.begin();
-			//update_addr(0x20, 0x21);
-			update();
+			//Wire.begin();
+			//update_addr(0x20, 0x20);
+			//update();
 		}
 
 		void update() override {
-			Wire.beginTransmission(0x20);
-			Wire.write(0x00); // 0x00 is hum
-			Wire.endTransmission();
-			delay(1100);
-			Wire.requestFrom(0x20, 2);
-			unsigned int t = Wire.read() << 8;
-			t = t | Wire.read();
-			ESP_LOGD("chirp", "Humidity is: %i", t);
-			publish_state(t);
+			//Wire.beginTransmission(0x20);
+			//Wire.write(0x00); // 0x00 is hum
+			//Wire.endTransmission();
+			//delay(1100);
+			//Wire.requestFrom(0x20, 2);
+			//unsigned int t = Wire.read() << 8;
+			//t = t | Wire.read();
+			//ESP_LOGD("chirp", "Humidity is: %i", t);
+			//publish_state(t);
 		}
 
 		void update_addr(int addr, int new_addr) {
